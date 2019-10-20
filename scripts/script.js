d968bf5f2b6436146e1348be7486730aafb1035e
dimonhawk123 <53188466+dimonhawk123@users.noreply.github.com>
2019-10-20 20:26:44 +0400

    test task for coding class 2.0 courses

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function toggle() {
+    if(document.getElementById("invisible_text").style.display != "block") {
+        document.getElementById("invisible_text").style.display = "block"
+    } else {
+        document.getElementById("invisible_text").style.display = "none"
+    }
+}
