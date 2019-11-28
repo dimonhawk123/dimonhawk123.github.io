b3ba5f757ec16c43356f1f33f3f6b47492db5d2f
dimonhawk123 <53188466+dimonhawk123@users.noreply.github.com>
2019-11-29 01:30:07 +0500

    Delete script.js

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-function toggle() {
-    if(document.getElementById("invisible_text").style.display != "block") {
-        document.getElementById("invisible_text").style.display = "block"
-    } else {
-        document.getElementById("invisible_text").style.display = "none"
-    }
-}
